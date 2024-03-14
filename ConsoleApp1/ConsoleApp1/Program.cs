73fba53f56dd1394538f4ccc9764b4692e99af18
s25729 <s25729@pjwstk.edu.pl>
2024-03-14 18:50:51 +0100

    Modification 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = { 1, 2, 3, 4, 5, 6, 7 };
+        double average = GetAverage(numbers);
+        Console.WriteLine(average);
+
+    }
+
+    static double GetAverage(int[] numbers)
+    {
+        double sum = 0.0;
+        foreach (var number in numbers)
+        {
+            sum += number;
+        }
+        return sum / numbers.Length;
+    }
+    
+  
+
+}
